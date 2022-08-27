ed9a71de5f923c6ecf04503d6db6853f426c5f6a
Yuki Yamamura <yuki.yamamura@icloud.com>
2022-08-27 14:45:16 +0900

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,44 @@
-import type { NextPage } from 'next';
+import capitalize from 'lodash/capitalize';
+import Head from 'next/head';
+import Link from 'next/link';
+import { List } from 'semantic-ui-react';
+import orgCodes from '@/data/org-codes';
+import type { NextPage, GetStaticProps } from 'next';
+import styles from '@/styles/pages/Home.module.css';
 
-const Home: NextPage = () => <h1> Hello world!</h1>;
+type Props = { orgCodes: string[] };
+
+export const getStaticProps: GetStaticProps<Props> = () => ({
+  props: {
+    orgCodes,
+  },
+});
+
+const Home: NextPage<Props> = ({ orgCodes }) => {
+  const title = '会社一覧';
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <header className="app-header">
+        <h1>{title}</h1>
+      </header>
+      <List celled relaxed>
+        {orgCodes.map((orgCode) => (
+          <List.Item className={styles['list-item']} key={orgCode}>
+            <List.Icon name="users" size="large" verticalAlign="middle" />
+            <List.Content>
+              <Link href={`/${orgCode}/members`}>
+                {`${capitalize(orgCode)} のメンバー`}
+              </Link>
+            </List.Content>
+          </List.Item>
+        ))}
+      </List>
+    </>
+  );
+};
 
 export default Home;
